5ccfe8d3c7fceedcd026fc6c1f18ffd342cb8a66
Brad Spencer <bradspencer@covenantchristian.org>
2018-09-10 20:20:05 -0400

    Delete config.js

--- src/services/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCg_FVHdzP3kvRAyrnpE2bJUsQfMRUgFW4',
-  authDomain: 'my-covenant.firebaseapp.com',
-  databaseURL: 'https://my-covenant.firebaseio.com',
-  projectId: 'my-covenant',
-  storageBucket: 'my-covenant.appspot.com',
-  messagingSenderId: '945207168321'
-}
