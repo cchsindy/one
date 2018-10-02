80849217674173602e36132101fe3e6a3c7b77e8
Brad Spencer <bradspencer@covenantchristian.org>
2018-10-02 09:32:54 -0400

    Firebase service

--- functions/services/blackbaud/BlackbaudService.js
+++ functions/services/blackbaud/BlackbaudService.js
@@ -1,181 +1,120 @@
 class BlackbaudService {
-  constructor() {}
+  constructor() {
+    this.ssh2 = require('ssh2')
+    this.Client = this.ssh2.Client
+    this.conn = new this.Client()
+    this.csv = require('csv-parse')
+    this.config = require('./config')
+    this.files = []
+    this.data = []
+  }
 
-  // import ssh2 from 'ssh2'
-  // import csv from 'csv-parse'
-  // import config from './blackbaud-config'
-  // import firebase from 'firebase/app'
-  // import 'firebase/firestore'
-  // import fbConfig from './firebase-config'
+  findSchedule(studentId) {
+    return new Promise((resolve, reject) => {
+      let ss = this.schedule.filter(s => s['Student ID'] === studentId)
+      resolve(ss)
+    })
+  }
 
-  // export default class Blackbaud {
-  //   constructor() {
-  //     const my = firebase.initializeApp(fbConfig.my, 'my')
+  async run() {
+    console.log('process started')
+    await this.openConnection()
+    await this.getFiles()
+    for await (const file of this.files) {
+      console.log('processing ' + file.filename)
+      this.getCSV(file.filename)
+      let collection = ''
+      switch (file.filename) {
+        case 'Faculty Records.CSV':
+          collection = 'bb_faculty'
+          break
+        case 'Individual Records.CSV':
+          collection = 'bb_individuals'
+          break
+        case 'Student Records.CSV':
+          collection = 'bb_students'
+          break
+      }
+      //this.saveToStore(collection)
+    }
+    await this.closeConnection()
+    console.log('process complete')
+    return
+  }
 
-  //     this.config = config
-  //     this.Client = ssh2.Client
-  //     this.conn = new this.Client()
-  //     this.csv = csv
-  //     this.files = []
-  //     this.data = []
-  //     this.myStore = my.firestore()
-  //     this.myStore.settings({ timestampsInSnapshots: true })
-  //   }
+  async getCSV(file) {
+    let self = this
+    await new Promise((resolve, reject) => {
+      this.conn.sftp((error, sftp) => {
+        if (error) {
+          reject(error)
+        }
 
-  //   findSchedule(studentId) {
-  //     return new Promise((resolve, reject) => {
-  //       let ss = this.schedule.filter(s => s['Student ID'] === studentId)
-  //       resolve(ss)
-  //     })
-  //   }
+        const rstream = sftp.createReadStream('/46157/data/' + file)
 
-  //   async run() {
-  //     console.log('process started')
-  //     await this.openConnection()
-  //     await this.getFiles()
-  //     for (const file of this.files) {
-  //       console.log('processing ' + file.filename)
-  //       await this.getCSV(file.filename)
-  //       let collection = ''
-  //       switch (file.filename) {
-  //         case 'Faculty Records.CSV':
-  //           collection = 'bb_faculty'
-  //           break
-  //         case 'Individual Records.CSV':
-  //           collection = 'bb_individuals'
-  //           break
-  //         case 'Student Records.CSV':
-  //           collection = 'bb_students'
-  //           break
-  //       }
-  //       await this.saveToStore(collection)
-  //     }
-  //     await this.closeConnection()
-  //     console.log('process complete')
-  //     return
-  //   }
+        let data = ''
+        rstream.on('data', chunk => {
+          data += chunk
+        })
+        rstream.on('end', () => {
+          self.csv(
+            data,
+            {
+              columns: true
+            },
+            (error, parsed) => {
+              if (error) {
+                reject(error)
+              }
+              self.data = parsed
+              resolve()
+            }
+          )
+        })
+        rstream.on('error', error => {
+          reject(error)
+        })
+      })
+    })
+  }
 
-  //   async saveToStore(collection) {
-  //     await new Promise((resolve, reject) => {
-  //       try {
-  //         const ts = Date.now()
-  //         for (const d of this.data) {
-  //           let docName = ''
-  //           let doc = {}
-  //           switch (collection) {
-  //             case 'bb_faculty':
-  //               docName = d['Record ID']
-  //               let faculty = (d['Current teacher?'] == 'Yes') ? true : false
-  //               doc = {
-  //                 email: d['Contact number'].trim().toLowerCase(),
-  //                 first: d['First name'].trim(),
-  //                 last: d['Last name'].trim(),
-  //                 title: d['Title 1'].trim(),
-  //                 faculty: faculty,
-  //                 synced: ts
-  //               }
-  //               break
-  //             case 'bb_individuals':
-  //               docName = d['Record ID']
-  //               doc = {
-  //                 email: d['Contact number'].trim().toLowerCase(),
-  //                 first: d['First name'].trim(),
-  //                 last: d['Last name'].trim(),
-  //                 username: d['Online user ID'].trim(),
-  //                 synced: ts
-  //               }
-  //               break
-  //             case 'bb_students':
-  //               docName = d['Student ID']
-  //               doc = {
-  //                 email: d['Contact number'].trim().toLowerCase(),
-  //                 first: d['First name'].trim(),
-  //                 last: d['Last name'].trim(),
-  //                 nickname: d['Nickname'].trim(),
-  //                 birthdate: d['Birth date'].trim(),
-  //                 gender: d['Gender'].trim(),
-  //                 grade: d['Current Grade'].trim(),
-  //                 status: d['Current status'].trim(),
-  //                 synced: ts
-  //               }
-  //               break
-  //           }
-  //           let docRef = this.myStore.doc(collection + '/' + docName)
-  //           docRef.set(doc)
-  //         }
-  //         resolve()
-  //       } catch (error) {
-  //         reject(error)
-  //       }
-  //     })
-  //   }
+  async getFiles() {
+    let self = this
+    await new Promise((resolve, reject) => {
+      this.conn.sftp((error, sftp) => {
+        if (error) {
+          reject(error)
+        }
+        sftp.readdir('/46157/data/', (error, list) => {
+          if (error) {
+            reject(error)
+          }
+          self.files = list
+          resolve()
+        })
+      })
+    })
+  }
 
-  //   async getCSV(file) {
-  //     let self = this
-  //     await new Promise((resolve, reject) => {
-  //       this.conn.sftp(function(error, sftp) {
-  //         if (error) {
-  //           reject('sftp error')
-  //         }
+  async closeConnection() {
+    await new Promise(resolve => {
+      this.conn.end()
+      resolve()
+    })
+  }
 
-  //         const rstream = sftp.createReadStream('/46157/data/' + file)
-
-  //         let data = ''
-  //         rstream.on('data', chunk => {
-  //           data += chunk
-  //         })
-  //         rstream.on('end', () => {
-  //           self.csv(data, { columns: true }, (error, parsed) => {
-  //             if (error) {
-  //               reject('error parsing csv')
-  //             }
-  //             self.data = parsed
-  //             resolve()
-  //           })
-  //         })
-  //         rstream.on('error', error => {
-  //           reject(error)
-  //         })
-  //       })
-  //     })
-  //   }
-
-  //   async getFiles() {
-  //     let self = this
-  //     await new Promise((resolve, reject) => {
-  //       this.conn.sftp(function(error, sftp) {
-  //         if (error) {
-  //           reject('sftp error')
-  //         }
-  //         sftp.readdir('/46157/data/', function(error, list) {
-  //           if (error) {
-  //             reject('readdir error')
-  //           }
-  //           self.files = list
-  //           resolve()
-  //         })
-  //       })
-  //     })
-  //   }
-
-  //   async closeConnection() {
-  //     await new Promise(resolve => {
-  //       this.conn.end()
-  //       resolve()
-  //     })
-  //   }
-
-  //   async openConnection() {
-  //     await new Promise((resolve, reject) => {
-  //       this.conn.on('ready', () => {
-  //         resolve()
-  //       }).connect(this.config)
-  //       this.conn.on('error', error => {
-  //         if (error) {
-  //           reject('not able to connect')
-  //         }
-  //       })
-  //     })
-  //   }
-  // }
+  async openConnection() {
+    await new Promise((resolve, reject) => {
+      this.conn
+        .on('ready', () => {
+          resolve()
+        })
+        .connect(this.config)
+      this.conn.on('error', error => {
+        if (error) {
+          reject(error)
+        }
+      })
+    })
+  }
 }
---
+++ functions/services/firebase/FirebaseService.js
@@ -0,0 +1,59 @@
+class FirebaseService {
+  constructor() {}
+
+  async saveToStore(collection) {
+    await new Promise((resolve, reject) => {
+      try {
+        const ts = Date.now()
+        for (const d of this.data) {
+          let docName = ''
+          let doc = {}
+          let faculty
+          switch (collection) {
+            case 'bb_faculty':
+              docName = d['Record ID']
+              faculty = d['Current teacher?'] === 'Yes' ? true : false
+              doc = {
+                email: d['Contact number'].trim().toLowerCase(),
+                first: d['First name'].trim(),
+                last: d['Last name'].trim(),
+                title: d['Title 1'].trim(),
+                faculty: faculty,
+                synced: ts
+              }
+              break
+            case 'bb_individuals':
+              docName = d['Record ID']
+              doc = {
+                email: d['Contact number'].trim().toLowerCase(),
+                first: d['First name'].trim(),
+                last: d['Last name'].trim(),
+                username: d['Online user ID'].trim(),
+                synced: ts
+              }
+              break
+            case 'bb_students':
+              docName = d['Student ID']
+              doc = {
+                email: d['Contact number'].trim().toLowerCase(),
+                first: d['First name'].trim(),
+                last: d['Last name'].trim(),
+                nickname: d['Nickname'].trim(),
+                birthdate: d['Birth date'].trim(),
+                gender: d['Gender'].trim(),
+                grade: d['Current Grade'].trim(),
+                status: d['Current status'].trim(),
+                synced: ts
+              }
+              break
+          }
+          let docRef = this.myStore.doc(collection + '/' + docName)
+          docRef.set(doc)
+        }
+        resolve()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
