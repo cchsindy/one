6152bccf0e43e75ae37d84331ce6aeea28e09d93
Brad Spencer <bradspencer@covenantchristian.org>
2019-11-13 16:48:21 -0500

    get sections or get courses?

--- src/views/Rosters.vue
+++ src/views/Rosters.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>Class Roster Emails</h1>
+    <h1>Course Roster Emails</h1>
+    <p v-for="course in courses" :key="course.course_code">
+      {{course.course_code}}: {{course.course_title}}
+    </p>
   </div>
 </template>
 
@@ -8,14 +11,14 @@
 export default {
   data: () => {
     return {
-      sections: null
+      courses: []
     }
   },
   methods: {
     loadSections() {
       const d = this.$store.state.fbFunctions.httpsCallable('skyapi')
-      getSections(d).then(data => {
-        this.sections = data
+      getCourses(d).then(data => {
+        this.courses = data
       })
     }
   },
@@ -24,15 +27,47 @@ export default {
   }
 }
 
+async function getCourses(f) {
+  const courses = await f({
+    product: 'school',
+    url: 'academics/courses',
+    params: { department_id: 34144, level_id: 2175 }
+  })
+  return courses.data.value
+}
+
 async function getSections(f) {
   const sections = await f({
     product: 'school',
-    url: 'levels',
-    params: {}
-    //params: { level_num: 1, school_year: '2019-2020' }
+    url: 'academics/sections',
+    params: { level_num: 2175, school_year: '2019-2020' }
   })
-  console.log(sections)
-  return sections.data
+  let courses = []
+  let prevCourse = ''
+  for (const s of sections.data.value) {
+    if (s.duration.name === '1st Semester') {
+      if (s.course_code != prevCourse) {
+        courses.push({
+          code: s.course_code,
+          sections: [
+            {
+              id: s.id,
+              name: s.name,
+              period: s.section_identifier
+            }
+          ]
+        })
+      } else {
+        courses[courses.length - 1].sections.push({
+          id: s.id,
+          name: s.name,
+          period: s.section_identifier
+        })
+      }
+      prevCourse = s.course_code
+    }
+  }
+  return courses
 }
 </script>
 
