4a7acbea49d2ee1c8c50aba0e6526bc204230206
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2020-03-06 14:31:05 -0500

    added name and photo

--- src/components/station/StationItem.vue
+++ src/components/station/StationItem.vue
@@ -3,6 +3,8 @@
     {{date}}:
     {{item.card}}
     - {{item.event}}
+    [ {{name}} ]
+    <img :src="image" />
   </div>
 </template>
 
@@ -11,12 +13,36 @@ export default {
   computed: {
     date() {
       return new Date(this.item.date);
+    },
+    name() {
+      const card = this.cards.filter(c => c.card === this.item.card);
+      if (card.length) {
+        return `${card[0].first} ${card[0].last}`;
+      }
+      return "Name not found.";
+    },
+    image() {
+      const card = this.cards.filter(c => c.card === this.item.card);
+      if (card.length && card[0].image) {
+        return `https://bbk12e1-cdn.myschoolcdn.com/ftpimages/1465/user/${card[0].image}`;
+      }
+      return "https://covenantchristian.myschoolapp.com/app/content/images/user.png";
     }
   },
   props: {
+    cards: {
+      type: Array,
+      required: true
+    },
     item: {
       type: Object,
       required: true
+    }
+  },
+  watch: {
+    item: {
+      handler: function() {},
+      deep: true
     }
   }
 };
@@ -28,5 +54,9 @@ export default {
   border: solid 2px #ccc;
   margin-top: 1vh;
   padding: 1vw;
+}
+img {
+  height: 72px;
+  width: 72px;
 }
 </style>
--- src/views/Station.vue
+++ src/views/Station.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h1>Charging Station</h1>
-    <BaseButton @click="downloadCSV">Download CSV</BaseButton>
+    <!-- <BaseButton @click="downloadCSV">Download CSV</BaseButton> -->
     <div>
-      <StationItem v-for="item in events" :key="item.date" :item="item" />
+      <StationItem v-for="item in events" :key="item.date" :item="item" :cards="cards" />
     </div>
   </div>
 </template>
 
 <script>
 // import { mapGetters, mapState } from 'vuex'
-import { mapState } from "vuex";
+// import { mapState } from "vuex";
 import StationItem from "@/components/station/StationItem";
 
 export default {
@@ -22,9 +22,17 @@ export default {
     //   'totalTickets',
     //   'totalSpecAmount'
     // ]),
-    ...mapState({
-      events: state => state.station.events
-    })
+    // ...mapState({
+    //   events: state => state.station.events
+    // })
+    events: function() {
+      return this.$store.state.station.events;
+    }
+  },
+  data: () => {
+    return {
+      cards: []
+    };
   },
   methods: {
     downloadCSV() {
@@ -60,9 +68,35 @@ export default {
     }
   },
   created() {
+    const d = this.$store.state.fbFunctions.httpsCallable("skyapi");
+    getCards(d).then(data => {
+      this.cards = data;
+    });
     this.$store.dispatch("fetchStationEvents");
   }
 };
+
+async function getCards(f) {
+  const result = await f({
+    product: "school",
+    url: "legacy/lists/99411",
+    params: {}
+  });
+  const cards = [];
+  for (const row of result.data.rows) {
+    cards.push({
+      userId: parseInt(row.columns[0].value),
+      first: row.columns[1].value,
+      last: row.columns[2].value,
+      email: row.columns[3].value,
+      nickname: row.columns[4].value,
+      role: row.columns[5].value,
+      card: parseInt(row.columns[6].value),
+      image: row.columns[7].value
+    });
+  }
+  return cards;
+}
 </script>
 
 <style scoped>
