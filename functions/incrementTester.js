f9d8a0682ce5a2204c1fab6312b12294119639eb
bradocchs <bradspencer@covenantchristian.org>
2020-08-02 17:59:27 -0400

    moved to separate project

--- functions/incrementTester.js
+++
@@ -1,9 +0,0 @@
-const FirestoreService = require('./services/firebase/FirestoreService')
-
-const fs = new FirestoreService()
-
-fs.documentFieldsIncrementer('test', {
-  one: 4,
-  two: -3,
-  three: 8
-})
