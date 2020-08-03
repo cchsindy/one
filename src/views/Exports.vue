6e278bf0cf177871123a657e217cbf37303e97e9
bradocchs <bradspencer@covenantchristian.org>
2020-08-02 20:03:54 -0400

    changed from on to sky

--- src/views/Exports.vue
+++ src/views/Exports.vue
@@ -14,7 +14,7 @@ import moment from "moment";
 export default {
   data: () => {
     return {
-      message: ""
+      message: "",
     };
   },
   methods: {
@@ -31,15 +31,12 @@ export default {
             ? moment(first).format("MM/DD/YYYY")
             : moment(s.started).format("MM/DD/YYYY");
         const end = moment(s.departed).format("MM/DD/YYYY");
-        let attended = schedule.filter(d => s.started <= d && s.departed >= d)
+        let attended = schedule.filter((d) => s.started <= d && s.departed >= d)
           .length;
         let excused =
-          attendance.filter(a => s.id === a.id && a.type === "E").length / 8;
+          attendance.filter((a) => s.id === a.id && a.type === "E").length / 8;
         let int = Math.trunc(excused);
-        let dec = excused
-          .toFixed(1)
-          .toString()
-          .slice(-1);
+        let dec = excused.toFixed(1).toString().slice(-1);
         let newDec = "0";
         if (dec > 3) newDec = "5";
         if (dec > 8) {
@@ -49,12 +46,9 @@ export default {
         excused = parseFloat(int.toString() + "." + newDec);
         attended -= excused;
         let unexcused =
-          attendance.filter(a => s.id === a.id && a.type === "U").length / 8;
+          attendance.filter((a) => s.id === a.id && a.type === "U").length / 8;
         int = Math.trunc(unexcused);
-        dec = unexcused
-          .toFixed(1)
-          .toString()
-          .slice(-1);
+        dec = unexcused.toFixed(1).toString().slice(-1);
         newDec = "0";
         if (dec > 3) newDec = "5";
         if (dec > 8) {
@@ -68,205 +62,214 @@ export default {
         );
       }
       let csvContent = "data:text/csv;charset=utf-8,";
-      doeAT.forEach(row => {
+      doeAT.forEach((row) => {
         csvContent += row + "\r\n";
       });
       const encodedUri = encodeURI(csvContent);
       window.open(encodedUri);
     },
     getNWEA() {
-      const d = this.$store.state.fbFunctions.httpsCallable("onapi");
+      const d = this.$store.state.fbFunctions.httpsCallable("skyapi");
       // list 91578 Sem 1 - 98868 Sem 2
-      d({ url: `list/98868`, params: {} }).then(result => {
-        // check for missing/invalid data
-        let rows = [
-          [
-            "School State Code",
-            "School Name",
-            "Previous Instructor ID",
-            "Instructor ID",
-            "Instructor State ID",
-            "Instructor Last Name",
-            "Instructor First Name",
-            "Instructor Middle Initial",
-            "User Name",
-            "Email Address",
-            "Class Name",
-            "Previous Student ID",
-            "Student ID",
-            "Student State ID",
-            "Student Last Name",
-            "Student First Name",
-            "Student Middle Initial",
-            "Student Date Of Birth",
-            "Student Gender",
-            "Student Grade",
-            "Student Ethnic Group Name",
-            "Student User Name",
-            "Student Email"
-          ]
-        ];
-        result.data.forEach(student => {
-          if (student.Ethnicity !== null) {
-            const bd = moment(student.Dob);
-            let grade = "9";
-            switch (student.GradeLevelDescription) {
-              case "Senior":
-                grade = "12";
-                break;
-              case "Junior":
-                grade = "11";
-                break;
-              case "Sophomore":
-                grade = "10";
-                break;
-              case "Freshman":
-                grade = "9";
-                break;
+      d({ product: "school", url: `legacy/lists/91578`, params: {} }).then(
+        (result) => {
+          // check for missing/invalid data
+          let rows = [
+            [
+              "School State Code",
+              "School Name",
+              "Previous Instructor ID",
+              "Instructor ID",
+              "Instructor State ID",
+              "Instructor Last Name",
+              "Instructor First Name",
+              "Instructor Middle Initial",
+              "User Name",
+              "Email Address",
+              "Class Name",
+              "Previous Student ID",
+              "Student ID",
+              "Student State ID",
+              "Student Last Name",
+              "Student First Name",
+              "Student Middle Initial",
+              "Student Date Of Birth",
+              "Student Gender",
+              "Student Grade",
+              "Student Ethnic Group Name",
+              "Student User Name",
+              "Student Email",
+            ],
+          ];
+          result.data.rows.forEach((row) => {
+            const student = row.columns;
+            if (student[10].value !== null) {
+              const bd = moment(student[9].value);
+              let grade = "9";
+              switch (student[16].value) {
+                case "Senior":
+                  grade = "12";
+                  break;
+                case "Junior":
+                  grade = "11";
+                  break;
+                case "Sophomore":
+                  grade = "10";
+                  break;
+                case "Freshman":
+                  grade = "9";
+                  break;
+              }
+              let r = [
+                "C527",
+                "Covenant Christian High School",
+                "",
+                student[15].value,
+                "",
+                student[12].value,
+                student[11].value,
+                "",
+                student[13].value,
+                student[13].value,
+                student[2].value + " - Period " + student[3].value,
+                "",
+                student[7].value,
+                "",
+                student[1].value,
+                student[0].value,
+                "",
+                bd.format("MM/DD/YYYY"),
+                student[8].value === "Female" ? "F" : "M",
+                grade,
+                student[10].value,
+                "",
+                "",
+              ];
+              rows.push(r);
             }
-            let r = [
-              "C527",
-              "Covenant Christian High School",
-              "",
-              student.SPN,
-              "",
-              student.LastName,
-              student.FirstName,
-              "",
-              student.EMail,
-              student.EMail,
-              student.CourseTitle + " - Period " + student.GroupIdentifier,
-              "",
-              student.GradeLevelDescription === "Freshman"
-                ? student.UserID
-                : student.HostID,
-              "",
-              student.StudentLastName,
-              student.StudentFirstName,
-              "",
-              bd.format("MM/DD/YYYY"),
-              student.Gender === "Female" ? "F" : "M",
-              grade,
-              student.Ethnicity,
-              "",
-              ""
-            ];
-            rows.push(r);
-          }
-        });
-        let csvContent = "data:text/csv;charset=utf-8,";
-        rows.forEach(rowArray => {
-          let row = rowArray.join(",");
-          csvContent += row + "\r\n";
-        });
-        var encodedUri = encodeURI(csvContent);
-        window.open(encodedUri);
-      });
+          });
+          let csvContent = "data:text/csv;charset=utf-8,";
+          rows.forEach((rowArray) => {
+            let row = rowArray.join(",");
+            csvContent += row + "\r\n";
+          });
+          var encodedUri = encodeURI(csvContent);
+          window.open(encodedUri);
+        }
+      );
     },
     getSTN() {
-      const d = this.$store.state.fbFunctions.httpsCallable("onapi");
-      d({ url: `list/91346`, params: {} }).then(result => {
-        const missing = result.data.filter(s => s.STN === null);
-        if (missing.length) {
-          this.message =
-            "<b><u>Students not exported - missing STN:</u></b><ul>";
-          for (const m of missing) {
-            this.message += `<li>${m.FirstName} ${m.LastName}</li>`;
+      const d = this.$store.state.fbFunctions.httpsCallable("skyapi");
+      d({ product: "school", url: `legacy/lists/91346`, params: {} }).then(
+        (result) => {
+          let missing = [];
+          let rows = [
+            [
+              "School Number",
+              "Student Test Number",
+              "Student Last Name",
+              "Student First Name",
+              "Language Code",
+              "Gender",
+              "Birth Date",
+              "Ethnicity/Race",
+            ],
+          ];
+          result.data.rows.forEach((row) => {
+            const student = row.columns;
+            if (student[6].value) {
+              let langCode = "";
+              switch (student[5].value) {
+                case "Afrikaans":
+                  langCode = "010";
+                  break;
+                case "Arabic":
+                  langCode = "040";
+                  break;
+                case "Czech":
+                  langCode = "175";
+                  break;
+                case "English":
+                  langCode = "211";
+                  break;
+                case "French":
+                  langCode = "250";
+                  break;
+                case "Korean":
+                  langCode = "485";
+                  break;
+                case "Mandarin":
+                  langCode = "600";
+                  break;
+                case "Spanish":
+                  langCode = "835";
+                  break;
+                case "Vietnamese":
+                  langCode = "945";
+                  break;
+              }
+              const bd = moment(student[3].value);
+              let eth = "5";
+              switch (student[4].value) {
+                case "American Indian/Alaskan Native":
+                  eth = "1";
+                  break;
+                case "Black":
+                  eth = "2";
+                  break;
+                case "Asian":
+                  eth = "3";
+                  break;
+                case "Hispanic/Latino":
+                  eth = "4";
+                  break;
+                case "White":
+                  eth = "5";
+                  break;
+                case "Multiracial":
+                  eth = "6";
+                  break;
+                case "Native Hawaiian/Pacific Islander":
+                  eth = "7";
+                  break;
+              }
+              let r = [
+                "C527",
+                student[6].value,
+                student[1].value,
+                student[0].value,
+                langCode,
+                student[2].value === "Female" ? "F" : "M",
+                bd.format("MM/DD/YYYY"),
+                eth,
+              ];
+              rows.push(r);
+            } else {
+              missing.push({
+                FirstName: student[0].value,
+                LastName: student[1].value,
+              });
+            }
+          });
+          if (missing.length) {
+            this.message =
+              "<b><u>Students not exported - missing STN:</u></b><ul>";
+            for (const m of missing) {
+              this.message += `<li>${m.FirstName} ${m.LastName}</li>`;
+            }
+            this.message += "</ul>";
           }
-          this.message += "</ul>";
+          let csvContent = "data:text/csv;charset=utf-8,";
+          rows.forEach((rowArray) => {
+            let row = rowArray.join(",");
+            csvContent += row + "\r\n";
+          });
+          var encodedUri = encodeURI(csvContent);
+          window.open(encodedUri);
         }
-        let rows = [
-          [
-            "School Number",
-            "Student Test Number",
-            "Student Last Name",
-            "Student First Name",
-            "Language Code",
-            "Gender",
-            "Birth Date",
-            "Ethnicity/Race"
-          ]
-        ];
-        result.data.forEach(student => {
-          if (student.STN !== null) {
-            let langCode = "";
-            switch (student.PrimaryLanguage) {
-              case "Afrikaans":
-                langCode = "010";
-                break;
-              case "Arabic":
-                langCode = "040";
-                break;
-              case "Czech":
-                langCode = "175";
-                break;
-              case "English":
-                langCode = "211";
-                break;
-              case "French":
-                langCode = "250";
-                break;
-              case "Korean":
-                langCode = "485";
-                break;
-              case "Mandarin":
-                langCode = "600";
-                break;
-              case "Spanish":
-                langCode = "835";
-                break;
-              case "Vietnamese":
-                langCode = "945";
-                break;
-            }
-            const bd = moment(student.Dob);
-            let eth = "5";
-            switch (student.Ethnicity) {
-              case "American Indian/Alaskan Native":
-                eth = "1";
-                break;
-              case "Black":
-                eth = "2";
-                break;
-              case "Asian":
-                eth = "3";
-                break;
-              case "Hispanic/Latino":
-                eth = "4";
-                break;
-              case "White":
-                eth = "5";
-                break;
-              case "Multiracial":
-                eth = "6";
-                break;
-              case "Native Hawaiian/Pacific Islander":
-                eth = "7";
-                break;
-            }
-            let r = [
-              "C527",
-              student.STN,
-              student.LastName,
-              student.FirstName,
-              langCode,
-              student.Gender === "Female" ? "F" : "M",
-              bd.format("MM/DD/YYYY"),
-              eth
-            ];
-            rows.push(r);
-          }
-        });
-        let csvContent = "data:text/csv;charset=utf-8,";
-        rows.forEach(rowArray => {
-          let row = rowArray.join(",");
-          csvContent += row + "\r\n";
-        });
-        var encodedUri = encodeURI(csvContent);
-        window.open(encodedUri);
-      });
-    }
-  }
+      );
+    },
+  },
 };
 
 // eslint-disable-next-line no-unused-vars
@@ -280,14 +283,14 @@ async function getDayAttendance(sky, days) {
       params: {
         level_id: 2175,
         day: moment(day).format("YYYY-MM-DD"),
-        offering_type: 1
-      }
+        offering_type: 1,
+      },
     });
-    result.data.value.forEach(r => {
+    result.data.value.forEach((r) => {
       if (r.excuse_type_id === 6736 || r.excuse_type_id === 7806) {
         attendance.push({
           id: r.student_user_id,
-          type: "U"
+          type: "U",
         });
       } else if (
         r.excuse_type_id === 6737 ||
@@ -296,7 +299,7 @@ async function getDayAttendance(sky, days) {
       ) {
         attendance.push({
           id: r.student_user_id,
-          type: "E"
+          type: "E",
         });
       }
     });
@@ -309,7 +312,7 @@ async function getMasterSchedule(sky, start, end) {
   const result = await sky({
     product: "school",
     url: "academics/schedules/master",
-    params: { level_num: 2175, start_date: start, end_date: end }
+    params: { level_num: 2175, start_date: start, end_date: end },
   });
   const master = [];
   // fix Fall Break dates
@@ -345,7 +348,7 @@ async function getMasterSchedule(sky, start, end) {
 async function getCurrentStudents(sky) {
   const result = await sky({
     product: "school",
-    url: "legacy/lists/102532"
+    url: "legacy/lists/102532",
   });
   const students = [];
   let prev = 0;
@@ -371,7 +374,7 @@ async function getCurrentStudents(sky) {
           started: new Date(r.columns[4].value),
           departed,
           grade,
-          name: r.columns[1].value + " " + r.columns[2].value
+          name: r.columns[1].value + " " + r.columns[2].value,
         });
       } else {
         if (r.columns[6].value && r.columns[5].value) {
@@ -396,7 +399,7 @@ async function getCurrentStudents(sky) {
               started: new Date(r.columns[4].value),
               departed,
               grade,
-              name: r.columns[1].value + " " + r.columns[2].value
+              name: r.columns[1].value + " " + r.columns[2].value,
             });
           }
         }
