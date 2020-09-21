9810b0daccffc89d15a3fbf59203c7ee4e100f32
bradocchs <bradspencer@covenantchristian.org>
2020-09-21 15:25:54 -0400

    updated to semester at CCHS

--- src/views/IHSAA.vue
+++ src/views/IHSAA.vue
@@ -20,13 +20,13 @@ export default {
   methods: {
     download() {
       let csv =
-        'data:text/csv;charset=utf-8,last,first,birthdate,grade,previousAttempted,previousEarned,currentAttempted\r\n'
+        'data:text/csv;charset=utf-8,last,first,birthdate,grade,semesters,previousEarned,currentAttempted\r\n'
       for (const s of this.students) {
         csv += s.last + ','
         csv += s.first + ','
         csv += s.birthdate + ','
         csv += s.grade + ','
-        csv += s.lastCredits + ','
+        csv += s.semesters + ','
         csv += s.lastPassed + ','
         csv += s.currentCredits + '\r\n'
       }
@@ -68,7 +68,13 @@ export default {
             grade,
             birthdate: moment(dob).format('MM/DD/YYYY'),
             currentCredits: parseInt(row.columns[5].value),
-            lastCredits: 0,
+            s2_1920: 0,
+            s1_1920: 0,
+            s2_1819: 0,
+            s1_1819: 0,
+            s2_1718: 0,
+            s1_1718: 0,
+            semesters: 0,
             lastPassed: 0,
           })
         }
@@ -83,9 +89,58 @@ export default {
           (s) => s.id === parseInt(row.columns[0].value)
         )
         if (student) {
-          student.lastCredits += parseInt(row.columns[2].value)
+          student.s2_1920 = 1
           student.lastPassed += parseInt(row.columns[1].value)
         }
+      }
+      const sem1 = await sky({
+        product: 'school',
+        url: 'legacy/lists/107835',
+        params: {},
+      })
+      for (const row of sem1.data.rows) {
+        const student = this.students.find(
+          (s) => s.id === parseInt(row.columns[0].value)
+        )
+        if (student) {
+          student.s1_1920 = 1
+        }
+      }
+      const trans = await sky({
+        product: 'school',
+        url: 'legacy/lists/107833',
+        params: {},
+      })
+      for (const row of trans.data.rows) {
+        const student = this.students.find(
+          (s) => s.id === parseInt(row.columns[8].value)
+        )
+        if (student) {
+          if (
+            row.columns[3].value === '2018 - 2019' &&
+            row.columns[7].value === '2nd'
+          )
+            student.s2_1819 = 1
+          if (
+            row.columns[3].value === '2018 - 2019' &&
+            row.columns[7].value === '1st'
+          )
+            student.s1_1819 = 1
+          if (
+            row.columns[3].value === '2017 - 2018' &&
+            row.columns[7].value === '2nd'
+          )
+            student.s2_1718 = 1
+          if (
+            row.columns[3].value === '2017 - 2018' &&
+            row.columns[7].value === '1st'
+          )
+            student.s1_1718 = 1
+        }
+      }
+      for (const s of this.students) {
+        s.semesters =
+          s.s2_1920 + s.s1_1920 + s.s2_1819 + s.s1_1819 + s.s2_1718 + s.s1_1718
       }
       this.showDownload = true
     },
