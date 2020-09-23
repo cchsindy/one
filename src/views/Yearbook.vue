2ef424cb3e3301682720edcb1252bbbad7c9ea98
Brad Spencer <bradspencer@Brads-iMac.attlocal.net>
2020-09-22 21:20:39 -0400

    wrap columns in quotes

--- src/views/Yearbook.vue
+++ src/views/Yearbook.vue
@@ -3,8 +3,8 @@
     <h1>Yearbook Orders</h1>
     <BaseButton @click="downloadCSV">Download CSV</BaseButton>
     <div class="transaction-totals">
-      <div class="total">Orders: {{totalYearbookOrders}}</div>
-      <div class="total">Total Amount: ${{totalYearbookAmount}}</div>
+      <div class="total">Orders: {{ totalYearbookOrders }}</div>
+      <div class="total">Total Amount: ${{ totalYearbookAmount }}</div>
     </div>
     <div class="transaction-list">
       <YearbookItem v-for="item in transactions" :key="item.id" :item="item" />
@@ -13,15 +13,15 @@
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
-import YearbookItem from "@/components/yearbook/YearbookItem";
+import { mapGetters, mapState } from 'vuex'
+import YearbookItem from '@/components/yearbook/YearbookItem'
 
 export default {
   components: {
     YearbookItem,
   },
   computed: {
-    ...mapGetters(["totalYearbookOrders", "totalYearbookAmount"]),
+    ...mapGetters(['totalYearbookOrders', 'totalYearbookAmount']),
     ...mapState({
       transactions: (state) => state.transactions.transactions,
     }),
@@ -30,52 +30,52 @@ export default {
     downloadCSV() {
       const rows = [
         [
-          "date",
-          "lastname",
-          "firstname",
-          "address",
-          "city",
-          "state",
-          "zip",
-          "email",
-          "phone",
-          "books",
-          "students",
-          "amount",
-          "paymentId",
+          'date',
+          'lastname',
+          'firstname',
+          'address',
+          'city',
+          'state',
+          'zip',
+          'email',
+          'phone',
+          'books',
+          'students',
+          'amount',
+          'paymentId',
         ],
-      ];
+      ]
       this.transactions.forEach((trans) => {
         let t = [
           new Date(trans.date).toDateString(),
-          trans.lastName,
-          trans.firstName,
-          trans.address,
-          trans.city,
-          trans.state,
-          trans.zip,
-          trans.email,
-          trans.phone,
-          trans.books,
+          `"${trans.lastName}"`,
+          `"${trans.firstName}"`,
+          `"${trans.address}"`,
+          `"${trans.city}"`,
+          `"${trans.state}"`,
+          `"${trans.zip}"`,
+          `"${trans.email}"`,
+          `"${trans.phone}"`,
+          `"${trans.books}"`,
           `"${trans.students}"`,
-          trans.amount,
-          trans.paymentId,
-        ];
-        rows.push(t);
-      });
-      let csvContent = "data:text/csv;charset=utf-8,";
-      rows.forEach(function (rowArray) {
-        let row = rowArray.join(",");
-        csvContent += row + "\r\n";
-      });
-      var encodedUri = encodeURI(csvContent);
-      window.open(encodedUri);
+          `"${trans.amount}"`,
+          `"${trans.paymentId}"`,
+        ]
+        rows.push(t)
+      })
+      let csvContent = 'data:text/csv;charset=utf-8,'
+      rows.forEach(function(rowArray) {
+        let row = rowArray.join(',')
+        csvContent += row + '\r\n'
+      })
+      var encodedUri = encodeURI(csvContent)
+      window.open(encodedUri)
     },
   },
   created() {
-    this.$store.dispatch("fetchYearbookOrders");
+    this.$store.dispatch('fetchYearbookOrders')
   },
-};
+}
 </script>
 
 <style scoped>
