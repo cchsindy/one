bcf914f8d5cd1ea373f4e4588ed2d8767a0f4923
bradocchs <bradspencer@covenantchristian.org>
2020-12-08 15:34:42 -0500

    back to get

--- src/views/AttendUpdate.vue
+++ src/views/AttendUpdate.vue
@@ -8,27 +8,27 @@
 export default {
   methods: {},
   mounted: async function() {
-    const sky = this.$store.state.fbFunctions.httpsCallable('skypost')
-    const result = await sky({
-      product: 'school',
-      url: 'attendance',
-      params: {
-        student_user_id: 5530730,
-        begin_date: '2020-11-30T00:00:00.000000+00:00',
-        end_date: '2020-11-30T00:00:00.000000+00:00',
-        excuse_type_id: 9105,
-      },
-    })
+    const sky = this.$store.state.fbFunctions.httpsCallable('skyapi')
     // const result = await sky({
     //   product: 'school',
     //   url: 'attendance',
     //   params: {
-    //     level_id: 2175,
-    //     day: '2020-11-30',
-    //     offering_type: 1,
-    //     // excuse_type: 9105,
+    //     student_user_id: 5530,
+    //     begin_date: '2020-11-30T00:00:00.000000+00:00',
+    //     end_date: '2020-11-30T00:00:00.000000+00:00',
+    //     excuse_type_id: 9105,
     //   },
     // })
+    const result = await sky({
+      product: 'school',
+      url: 'attendance',
+      params: {
+        level_id: 2175,
+        day: '2020-11-30',
+        offering_type: 1,
+        // excuse_type: 9105,
+      },
+    })
     console.log(result)
   },
 }
